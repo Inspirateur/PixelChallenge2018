d2d46d70f72846c62a07006de34d6b3bc9ba22c8
MrFouss <maxime.brodat@fouss.fr>
2018-04-06 10:20:53 -0400

    Add basic custom gravity

---
+++ Assets/Scripts/Environment/CustomGravity.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomGravity : MonoBehaviour
+{
+
+    public CustomGravityData GravityData;
+    
+    private Rigidbody2D rb;
+    
+	void Start ()
+    {
+        rb = GetComponentInChildren<Rigidbody2D>();
+	}
+	
+	void FixedUpdate ()
+    {
+        Vector2 forceToApply = transform.position * GravityData.ForceValue * rb.mass;
+        if (GravityData.IsAttraction)
+        {
+            forceToApply *= -1f;
+        }
+
+        rb.AddForce(forceToApply);
+
+        if (GravityData.IsRotatedTowardsCenter)
+        {
+            transform.Rotate(Vector3.forward, Vector3.SignedAngle(transform.up, -1f * transform.position, Vector3.forward));
+        }
+	}
+}
