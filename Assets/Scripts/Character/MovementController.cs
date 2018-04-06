fac91e57cd979fecf81434d06742fc874032a320
MrFouss <maxime.brodat@fouss.fr>
2018-04-06 13:58:33 -0400

    Add character lateral movement

--- Assets/Scripts/Character/MovementController.cs
+++ Assets/Scripts/Character/MovementController.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementController : MonoBehaviour {
+public class MovementController : MonoBehaviour
+{
+
+    public CharacterMovementData MovementData;
+
+    private Rigidbody2D rb;
     
 	void Start ()
     {
-		
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
-	void Update ()
+	void FixedUpdate ()
     {
-		
+        float accelerationFactor = MovementData.AccelerationCurve.Evaluate(rb.velocity.magnitude / MovementData.MaxSpeed);
+        rb.velocity += new Vector2(transform.right.x, transform.right.y) * MovementData.Acceleration * accelerationFactor * Time.deltaTime;
+        Debug.Log(rb.velocity.magnitude + " ; " + accelerationFactor);
 	}
 }
