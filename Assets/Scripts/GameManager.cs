0101891b27375ff615d9dff5ccc5b294839258a9
Lucas <kitolex.soluce@gmail.com>
2018-04-06 15:25:42 -0400

    changement niveau

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public float maginitudeVitesseJoueurTemp;
+
+	public float magnitudeAugmentationAChaqueNiveau;
+	private float magnitudeVitesseObjectif;
+
+	private Tempete tempete;
+
+	// Use this for initialization
+	void Start () {
+		tempete = Tempete.getInstance ();
+		skipLevel ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyUp (KeyCode.U)) {
+			maginitudeVitesseJoueurTemp++;
+		}
+
+		if(this.magnitudeVitesseObjectif<=this.getVitesseJoueur()){
+			skipLevel ();
+		}
+	}
+
+	private void skipLevel(){
+		Debug.Log ("SkipLevel");
+		this.magnitudeVitesseObjectif = getVitesseJoueur () + magnitudeAugmentationAChaqueNiveau;
+	}
+
+
+	private float getVitesseJoueur(){
+		return maginitudeVitesseJoueurTemp;
+	}
+}
