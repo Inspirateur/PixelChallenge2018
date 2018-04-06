033cbc38427c13b6ba7c982cac822421f06ca996
Ezia <esia.belbachir@protonmail.com>
2018-04-06 13:31:52 -0400

    Implement ground neon

---
+++ Assets/Scripts/Data/LightFlickerData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LightFlickerData", menuName = "Environment / LightFlicker")]
+public class LightFlickerData : ScriptableObject {
+
+    public float FlickerProbability = 1;
+    public float FlickerFrequency = 1;
+
+    // Must be less than FlickerFrequency
+    public float OffTime = 0.1f;
+}
---
+++ Assets/Scripts/Environment/LightFlicker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour {
+
+    public SpriteRenderer LightSprite;
+
+    public LightFlickerData Data;
+
+    private void Start()
+    {
+        SwitchOn();
+    }
+
+    private void SwitchOn()
+    {
+        LightSprite.color = new Color(1, 1, 1, 1);
+        if (Random.Range(0.0f,1.0f) <= Data.FlickerProbability)
+        {
+            Invoke("SwitchOff", Data.FlickerFrequency);
+        } else
+        {
+            Invoke("SwitchOn", Data.FlickerFrequency);
+        }
+    }
+
+    private void SwitchOff() {
+        LightSprite.color = new Color(0, 0, 0, 0);
+        Invoke("SwitchOn", Data.OffTime);
+    }
+}
