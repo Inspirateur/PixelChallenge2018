c6d892da9bb12e7bcb6ffa7e135fcf1b2efddc32
Ezia <esia.belbachir@protonmail.com>
2018-04-07 14:20:53 -0400

    Change particle emission color

--- Assets/Scripts/Environment/Neon.cs
+++ Assets/Scripts/Environment/Neon.cs
@@ -20,6 +20,7 @@ public class Neon : MonoBehaviour {
                     if (sys != null)
                     {
                         sys.GetComponent<Renderer>().material.SetColor("_Color", value);
+                        sys.GetComponent<Renderer>().material.SetColor("_EmissionColor", 0.4f * value);
                     } else { 
                         Debug.Log(t.tag);
                         t.gameObject.GetComponent<Neon>().CircleColor = value;
