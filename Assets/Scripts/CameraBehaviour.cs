82d4353c7cb1ea6e1ffd3717233449dcad370506
Fiesta87 <jb@pommeret.fr>
2018-04-07 09:44:53 -0400

    modif de la caméra et de la gravité du endGame

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -7,13 +7,16 @@ public class CameraBehaviour : MonoBehaviour {
 	private GameManager gm;
 
 	private float velocity = 0.0f ;
+	private Vector3 velocityEnd = Vector3.zero ;
 	private float distanceFocusDesiree;
 	private float initial;
+	private Vector3 posInitial;
 
 	// Use this for initialization
 	void Start () {
 		gm = GameObject.FindGameObjectWithTag ("GameManager").GetComponent<GameManager> ();
 		initial = this.transform.position.z;
+		posInitial = this.transform.position;
 	}
 
 	// Update is called once per frame
@@ -22,6 +25,15 @@ public class CameraBehaviour : MonoBehaviour {
 	}
 
 	void LateUpdate(){
+		
+		if(!gm.gameover){
+			UpdateGame();
+		} else {
+			UpdateEndGame();
+		}
+	}
+
+	private void UpdateGame(){
 		transform.up = Vector3.up;
 		distanceFocusDesiree = (gm.getCircleCourant ().ObjectDistance * 0.5f) + gm.getPosPlayer().z;
 	
@@ -30,10 +42,19 @@ public class CameraBehaviour : MonoBehaviour {
 		pos.z = Mathf.SmoothDamp (this.transform.position.z, gm.getPercent () * (-distanceFocusDesiree) + initial, ref velocity, 0.3f);
 
 		this.transform.position = pos;
-
 	}
 
+	private void UpdateEndGame(){
+		transform.up = Vector3.up;
+		distanceFocusDesiree = (gm.getCircleCourant ().ObjectDistance * 2.0f);
+	
+		Vector3 posDesire = posInitial;
+		posDesire.z -= distanceFocusDesiree;
 
+		Vector3 pos = Vector3.SmoothDamp (this.transform.position, posDesire, ref velocityEnd, 1.0f, 1.0f);
+
+		this.transform.position = pos;
+	}
 }
 
 //Camera.main.transform.position = Vector3.SmoothDamp (Camera.main.transform.position, pos, ref velocity, 0.01f,dureeAcces);
